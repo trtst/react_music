fbe0382c4c546623de9bd03bdcf6f68fe3893f75
yangpenghui <569615439@qq.com>
2023-09-14 01:57:44 +0800

    ~

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -2,12 +2,13 @@ import React, { memo } from 'react';
 import { Link } from "react-router-dom";
 import Menu from '@components/menu'
 import sideSty from './index.module.scss';
+import LOGO from '@assets/img/logo.jpg';
 
 export default memo(function Sidebar() {
     console.log('Sidebar render');
     return (
         <div className={sideSty.sidebar}>
-            <Link className={sideSty.logo} to='/'><img src={`/src/assets/img/logo.jpg`} /></Link>
+            <Link className={sideSty.logo} to='/'><img src={LOGO} /></Link>
             <Menu />
         </div>
     )
--- src/view/index/index.jsx
+++ src/view/index/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Banner from './banner';
 import Hot from './hot';
 import Album from './album';
@@ -8,14 +8,8 @@ import Dj from './dj';
 import Singer from './singer';
 
 export default function Index() {
-    const [ count, setCount ] = useState(0);
-
-    const add = () => {
-        setCount(count => count + 1);
-    }
     return (
         <>
-            <div onClick={add}>当前数字： {count}</div>
             <Banner />
             <Hot />
             <Album />
